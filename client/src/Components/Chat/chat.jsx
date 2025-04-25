0c473574c87f9adb7caab69e6adf53e44b0173b3
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-04-25 21:01:01 +0530

    Basic Notification Implementation

--- client/src/Components/Chat/chat.jsx
+++ client/src/Components/Chat/chat.jsx
@@ -7,13 +7,45 @@ function Chat() {
   const socket = socketIO(ENDPOINT, { transports: ["websocket"] });
   useEffect(() => {
     socket.on("connect", () => {
-      alert(`Connected ${user} with Socket ID- ${socket.id}`);
+      alert(`Connected ${user}`);
     });
-
-    return () => {};
+    socket.emit("joined", { user });
+    socket.on("welcome", (data) => {
+      console.log(`${data.user} ${data.msg} `);
+    });
+    socket.on("userjoined", (data) => {
+      console.log(`${data.user} ${data.msg} `);
+    });
+    socket.on("leaved", (data) => {
+      console.log(`${data.user} ${data.msg}`);
+    });
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
   }, []);
 
-  return <div>{user}</div>;
+  return (
+    <>
+      <div className="bg-black h-screen w-screen flex flex-col items-center justify-center gap-5">
+        <div className="flex flex-col items-center w-[50%] h-[50%] sm:w-[50%] text-white rounded-lg  bg-white">
+          <div className="bg-purple-300 h-[15%] w-full">Header</div>
+          <div className=" h-[70%] w-full box-border border-black border-2">
+            CahtBox
+          </div>
+          <div className="h-[15%] w-full box-border flex ">
+            <input
+              type="text"
+              className="flex text-black w-[80%] outline-none p-3"
+            />
+            <button className="cursor-pointer w-[20%] rounded border-none bg-purple-300 font-bold text-xl text-purple-600 hover:bg-purple-700 hover:text-white">
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -5,11 +5,32 @@ const socketIO = require("socket.io");
 
 const PORT = 8000;
 const app = express();
+const users = [];
+
 app.use(cors({ origin: "*", Credentials: true }));
 const server = http.createServer(app);
 const io = socketIO(server);
 io.on("connection", (socket) => {
   console.log(`New Connection - ${socket.id}`);
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit("userjoined", {
+      user: "Admin",
+      msg: `${users[socket.id]} has Joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      msg: `WELCOME to the Chat ${users[socket.id]}`,
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leaved", {
+      user: "Admin",
+      msg: `${users[socket.id]} left the chat`,
+    });
+    console.log(`${users[socket.id]} Left`);
+  });
 });
 server.listen(PORT, () => {
   console.log(`Connected @ PORT ${PORT}`);
